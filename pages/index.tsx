e32ff20c432ebd35a0f20b48d2e26a10ccf802f5
Tommy AngeloBTC <s@swinterface.io>
2021-08-16 23:53:40 +0300

    Adjust app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,207 +1,402 @@
+import React from "react";
 import Head from "next/head";
 
+import { Subject } from "rxjs";
+import { isNil } from "lodash";
+import { debounceTime } from "rxjs/operators";
+
+import { IDEActionSection } from "components/core/ActionSection";
+import { MainLogConsole } from "components/core/editing/LogConsole";
+
+import {
+  FileSystemEntity,
+  FileSystemEntityKind,
+  FileSystemEntityMeta,
+  FileSystemSnake,
+} from "parser/file";
+
+import {
+  HomeContainer,
+  Footer,
+  ApplicationColor,
+  FilesystemSection,
+  IDEActionsFlex,
+  LogoContainer,
+  MainTabSection,
+  ViewSelectSection,
+} from "components/main/styled";
+import {
+  MainEditingBody,
+  EditingBodyProps,
+} from "components/core/editing/EditBody";
+
+import { MediumHeading } from "components/common/Text/styled";
+
+import { ExplorerFileSystem } from "components/core/fs/ExplorerFileSystem";
+import {
+  CompilerSection,
+  CompilerProps,
+} from "components/core/compiler/CompilerSection";
+
+import {
+  ExplorerHeadingContainer,
+  FileExplorerHeading,
+  FileExplorerSubHeading,
+  ExplorerWorkspaceLabel,
+} from "components/core/fs/ExplorerFileSystem/styled";
+import { SessionClient } from "services/fs";
+import { LocalStorageManager, useLocalStorage } from "hooks/localStorage";
+import {
+  CompilationInfo,
+  SessionLegacyNode,
+  SessionTree,
+} from "protobuf/session";
+import { UpdateSessionBody } from "protobuf/transport";
+
+const sourceCodeFixture = require("fixtures/sc.json") as { sc: string };
+
+enum SectionType {
+  FileExplorers = 0,
+  Compiler,
+  Transactions,
+}
+
+type EditableFileSystem = Record<string, string>;
+
+type FileSystemState = {
+  state: EditableFileSystem;
+  filesystem: FileSystemSnake;
+};
+
+type Section = { text: string };
+const ideSectionList: Section[] = [
+  {
+    text: "File Explorers",
+  },
+  {
+    text: "Compiler",
+  },
+  // {
+  //   text: "Transactions",
+  // },
+];
+
+const sessionClient = new SessionClient({ endpoint: "http://localhost:8081" });
+
 export default function Home() {
+  const [compileInfo, setCompileInfo] = React.useState<CompilationInfo | null>(
+    null
+  );
+  const [isCompilationProcessing, setIsCompilationProcessing] =
+    React.useState(false);
+  const [compileCodeSubject] = React.useState(new Subject<null>());
+
+  React.useEffect(() => {
+    const subscription = compileCodeSubject
+      .pipe(debounceTime(300))
+      .subscribe(async () => {
+        setIsCompilationProcessing(true);
+        setCompileInfo(null);
+
+        const compilationInfo = await sessionClient.compileSessionCode(
+          sessionId
+        );
+
+        setCompileInfo(compilationInfo);
+        setIsCompilationProcessing(false);
+      });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const [codeUpdateSubject] = React.useState(
+    new Subject<{ [x: string]: string }>()
+  );
+  React.useEffect(() => {
+    const subscription = codeUpdateSubject
+      .pipe(debounceTime(300))
+      .subscribe(async (updateEntry) => {
+        const firstKey = Object.keys(updateEntry)[0];
+        const payload = Object.assign({}, updateEntry);
+
+        const buff = Buffer.from(payload[firstKey]);
+        const base64data = buff.toString("base64");
+
+        payload[firstKey] = base64data;
+
+        await sessionClient.updateSessionCode(
+          UpdateSessionBody.fromJSON({
+            session_id: sessionId,
+            tree: payload,
+          })
+        );
+      });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const [currentSection, setCurrentSection] = React.useState(
+    SectionType.FileExplorers
+    // SectionType.Compiler
+  );
+
+  const [currentSessionTree, setSessionTree] =
+    React.useState<SessionTree | null>(null);
+  const [currentSessionLegacyTree, setSessionLegacyTree] = React.useState<
+    SessionLegacyNode[] | null
+  >(null);
+
+  const [sessionId, setSessionValue] = useLocalStorage(
+    LocalStorageManager.SESSION_ID,
+    null
+  );
+
+  React.useEffect(() => {
+    (async () => {
+      const session = await sessionClient.fetchSessionCodeTree(sessionId);
+      const sessionLegacy = await sessionClient.fetchSessionCodeLegacyTree(
+        sessionId
+      );
+
+      setSessionTree(session);
+      setSessionLegacyTree(sessionLegacy);
+    })();
+  }, []);
+
+  React.useEffect(() => {
+    (async () => {
+      if (sessionId === null) {
+        const freshSessionId = await sessionClient.initNewSession();
+        setSessionValue(freshSessionId.session_id);
+      }
+    })();
+  }, [sessionId]);
+
+  const [currentSelectedFile, setCurrentSelectedFile] =
+    React.useState<FileSystemEntity | null>(null);
+
+  const [sourceCodeState, setSourceCodeState] =
+    React.useState<EditableFileSystem>({});
+
+  React.useEffect(() => {
+    if (isNil(currentSessionTree)) {
+      return;
+    }
+
+    const keys = Object.keys(currentSessionTree.file_paths);
+    for (let i = 0; i < keys.length; i++) {
+      const splittedPath = keys[i].split("/");
+      const path = splittedPath.slice(1).join("/");
+
+      const value = currentSessionTree.file_paths[keys[i]];
+      const buff = Buffer.from(value, "base64");
+      const text = buff.toString("utf8");
+
+      const newSourceCode = sourceCodeState;
+      newSourceCode[path] = text;
+      setSourceCodeState(newSourceCode);
+    }
+  }, [currentSessionTree]);
+
+  const [filesystemStructure, setFileSystemStructure] =
+    React.useState<FileSystemSnake | null>(null);
+
+  const editableFileSystem: FileSystemState = {
+    state: sourceCodeState,
+    filesystem: filesystemStructure,
+  };
+
+  React.useEffect(() => {
+    (async () => {
+      if (isNil(currentSessionLegacyTree)) {
+        return;
+      }
+      const result: FileSystemEntity[] = [];
+
+      const populate = (
+        inputFs: SessionLegacyNode[] | null,
+        levelCache: FileSystemEntity[]
+      ) => {
+        if (isNil(inputFs)) {
+          return;
+        }
+
+        for (let i = 0; i < inputFs.length; i++) {
+          const entity = inputFs[i];
+
+          if (entity.is_file) {
+            levelCache.push(FileSystemEntity.new_file({ name: entity.name }));
+          } else {
+            const thisCache = [];
+
+            populate(entity.children, thisCache);
+
+            levelCache.push(
+              FileSystemEntity.dir_with_entities(
+                { name: entity.name },
+                thisCache
+              ).populateDepth()
+            );
+          }
+        }
+      };
+      populate(currentSessionLegacyTree, result);
+
+      setFileSystemStructure(new FileSystemSnake(result));
+    })();
+  }, [currentSessionLegacyTree]);
+
+  const currentEntityFilePath = currentSelectedFile?.currentEntityFilePath;
+
+  const editingBodyProps: EditingBodyProps = {
+    compileInfo,
+    sourceCode: sourceCodeState[currentEntityFilePath],
+    filename: currentSelectedFile?.currentEntityFilePath,
+    onSourceCodeChange: (newCode) => {
+      const newState = Object.assign({}, sourceCodeState, {
+        [currentEntityFilePath]: newCode,
+      });
+
+      setSourceCodeState(newState);
+
+      codeUpdateSubject.next({
+        [`${sessionId}/${currentEntityFilePath}`]: newCode,
+      });
+      // await sessionClient.
+    },
+  };
+
+  const compilerProps: CompilerProps = {
+    currentFile: currentSelectedFile,
+    isProcessing: isCompilationProcessing,
+    compileInfo,
+    onCompile: () => {
+      compileCodeSubject.next(null);
+    },
+    downloadLink: sessionClient.buildDownloadSessionCompileCodeLink(sessionId)
+  };
+
+  const sectionsMapping = {
+    [SectionType.FileExplorers]: (
+      <FilesystemSection className="separator">
+        <ExplorerHeadingContainer>
+          <FileExplorerHeading>File Explorers</FileExplorerHeading>
+          <FileExplorerSubHeading>Workspaces</FileExplorerSubHeading>
+        </ExplorerHeadingContainer>
+        <ExplorerWorkspaceLabel>default_workspace</ExplorerWorkspaceLabel>
+        {!isNil(editableFileSystem.filesystem) && (
+          <ExplorerFileSystem
+            filesystem={editableFileSystem.filesystem}
+            onSelectFile={(filename) => setCurrentSelectedFile(filename)}
+          />
+        )}
+      </FilesystemSection>
+    ),
+    [SectionType.Compiler]: (
+      <FilesystemSection className="separator">
+        <ExplorerHeadingContainer>
+          <FileExplorerHeading>Solana Compiler</FileExplorerHeading>
+          {/* <FileExplorerSubHeading>Workspaces</FileExplorerSubHeading> */}
+
+          <CompilerSection {...compilerProps} />
+        </ExplorerHeadingContainer>
+      </FilesystemSection>
+    ),
+  };
+  const currentSectionComponent = sectionsMapping[currentSection];
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>SWI | Solana Web Interface</title>
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <HomeContainer>
+        <ViewSelectSection className="separator">
+          <LogoContainer>
+            <img src="/app/logo.svg" />
+            <MediumHeading>SWI</MediumHeading>
+          </LogoContainer>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <IDEActionsFlex>
+            {ideSectionList.map((x, i) => (
+              <IDEActionSection
+                key={x.text}
+                checked={i === currentSection}
+                onSelect={() => i >= 0 && i <= 1 && setCurrentSection(i)}
+              >
+                {x.text}
+              </IDEActionSection>
+            ))}
+          </IDEActionsFlex>
+        </ViewSelectSection>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
+        {currentSectionComponent}
+        <MainTabSection>
+          {!isNil(currentSelectedFile) && (
+            <MainEditingBody {...editingBodyProps} />
+          )}
+        </MainTabSection>
+      </HomeContainer>
+      <Footer />
 
       <style jsx global>{`
         html,
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: Poppins, -apple-system, BlinkMacSystemFont, Segoe UI,
+            Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+            Helvetica Neue, sans-serif;
+          background-color: ${ApplicationColor.MAIN_BG};
+        }
+
+        .container {
+          height: 100vh;
+        }
+
+        .separator {
+          border-right: 1px solid rgba(255, 255, 255, 0.1);
+        }
+
+        hr.simple {
+          border: none;
+          height: 1px;
+          background-color: rgba(255, 255, 255, 0.1);
+          width: 100%;
         }
 
         * {
           box-sizing: border-box;
         }
+
+        @import url(//fonts.googleapis.com/css?family=Poppins);
       `}</style>
     </div>
   );
